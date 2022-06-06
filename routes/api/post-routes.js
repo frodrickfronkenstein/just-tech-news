c207fa4ed80eae4636c75d9280084065210fbc34
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-06-05 21:25:41 -0400

    comments added

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,19 +1,26 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
-    console.log('=======================');
     Post.findAll({
-        attributes: ['id',
+        attributes: [
+        'id',
         'post_url',
         'title',
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
